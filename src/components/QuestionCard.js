49111806a2c2b1ac7130be4d351f339bc43a8143
Fleur41 <Muriithikings456@gmail.com>
2023-10-12 21:52:13 +0300

    errors

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
-const QuestionCard = () => {
-  return (
-    <div>QuestionCard</div>
-  )
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNr: number;
+    totalQuestions: number;
 }
+const QuestionCard: React.FC<Props> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNr,
+    totalQuestions,
+    }) => (
+     <div>
+        <p className='number'>
+            Question: {questionNr} / {totalQuestions}
+        </p>
+        <p dangerouslySetHTML={{ __html: question}}  / >
+        <div>
+            {answers.map(answer => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html: answer}} />
+                    </button>
+                </div>
+            ))}
+        </div>
+     </div>
+  );
+
 
 export default QuestionCard
